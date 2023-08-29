257f37d0588930aa6609f35167c059b865ed5b96
Shirel01 <shirel.israel2001@gmail.com>
2023-08-29 15:02:17 +0300

    change in main function

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from camera import capture_5_images
-import identify_situation
+import camera
+ #import identify_situation
 
 
 def main():
     while True:
         num_childs = input("Enter number of babies in the class: ")
-        images = capture_5_images()
+        images = camera.capture_5_images()
 
         if identify_rolling(images, num_childs):# if true there is a danger
             pass
