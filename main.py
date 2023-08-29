89fd201d5e217e15f9e62eee85ca0e06a757ef79
Shirel01 <shirel.israel2001@gmail.com>
2023-08-29 14:46:10 +0300

    add main function

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
-
-import camera
-
-
 from camera import capture_5_images
 import identify_situation
-
 
 
 def main():
     while True:
         num_childs = input("Enter number of babies in the class: ")
+        images = capture_5_images()
 
-        images = camera.capture_5_images()
-
-
-        if (identify_rolling(images, num_childs)):# if true there is a danger
+        if identify_rolling(images, num_childs):# if true there is a danger
             pass
             #send_image according to how we send the signal
 
