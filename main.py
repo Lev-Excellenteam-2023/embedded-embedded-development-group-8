eebfde3a60fe18867045f49b6fc1121209489914
Shirel01 <shirel.israel2001@gmail.com>
2023-08-29 21:25:35 +0300

    resolve conflict

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+
+import camera
+
+
 from camera import capture_5_images
 import identify_situation
+
 
 
 def main():
     while True:
         num_childs = input("Enter number of babies in the class: ")
-        images = capture_5_images()
+
+        images = camera.capture_5_images()
+
 
         if (identify_rolling(images, num_childs)):# if true there is a danger
             pass
